a299a47f68f2e84c67fb864de3b9308e2bff038f
Jumaiswit16 <Jumaibeautyandbeyond@gmail.com>
2022-09-16 13:51:36 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _isupper: returns an integar value basedon input
+ *_isupper - returns an integar value basedon input
  *
- * Return: 0 if input is lowercase
- * 	   1 if input is uppercase
+ *@c:is a varable
+ *Return: 0 if input is lowercase
+ *	1 if input is uppercase
  */
 
 int _isupper(int c)
 {
-	if(c >='a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (0);
 	}
-	else if (c >='A' && c <= 'Z')
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
